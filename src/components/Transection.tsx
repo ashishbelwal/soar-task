8b74d1c7bd3327f05b075807349015d951fcac5b
Ashish Belwal <ashish.ave@gmail.com>
2025-02-28 15:09:52 +0530

    fix url issues in transection

--- src/components/Transection.tsx
+++ src/components/Transection.tsx
@@ -5,17 +5,17 @@ export const Transaction = (transaction: Transection) => {
     <div className="flex w-full flex-row items-center rounded-full gap-[10px] lg:gap-[16px]">
       {transaction.source === "card" && (
         <div className="w-[50px] h-[50px] lg:w-[55px] lg:h-[55px] bg-[#FFF5D9] rounded-full flex justify-center items-center">
-          <img src="/public/icons/card.svg" alt="" />{" "}
+          <img src="/icons/card.svg" alt="" />{" "}
         </div>
       )}
       {transaction.source === "paypal" && (
         <div className="w-[50px] h-[50px] lg:w-[55px] lg:h-[55px] bg-[#E7EDFF] rounded-full flex justify-center items-center">
-          <img src="/public/icons/paypal.svg" alt="" />{" "}
+          <img src="/icons/paypal.svg" alt="" />{" "}
         </div>
       )}
       {transaction.source === "others" && (
         <div className="w-[50px] h-[50px] lg:w-[55px] lg:h-[55px] bg-[#DCFAF8] rounded-full flex justify-center items-center">
-          <img src="/public/icons/doller.svg" alt="" />{" "}
+          <img src="/icons/doller.svg" alt="" />{" "}
         </div>
       )}
 
